fa5ac4736f2f9bee444fe63e16aced3f07d43ac8
Joel Gonzales Tipismana <joelengt@gmail.com>
2016-03-04 14:50:37 -0500

    config cambiado

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://198.211.116.198',
+        url: 'http://joelgt.com',
         mail: {
             transport: 'SMTP',
                 options: {
